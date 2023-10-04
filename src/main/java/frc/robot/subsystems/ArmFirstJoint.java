7ff6550596021c077a6bcb271073cb8bca4b085b
Yoel-Perman-Brilliant <yoyopermam@gmail.com>
2023-10-04 22:38:14 +0300

    removed test

--- src/main/java/frc/robot/subsystems/ArmFirstJoint.java
+++ src/main/java/frc/robot/subsystems/ArmFirstJoint.java
@@ -154,8 +154,6 @@ public class ArmFirstJoint extends SparkMaxGenericSubsystem {
         namespace.putNumber("absolute encoder position", this::getAbsolutePosition);
         namespace.putNumber("spark max encoder position", this::getRelativePosition);
         namespace.putNumber("velocity", this::getVelocity);
-        namespace.putData("test", new MoveSmartMotorControllerGenericSubsystem(this,
-                pidSettings, feedForwardSettings, UnifiedControlMode.VELOCITY, () -> 0.0));
         namespace.putNumber("voltage", () -> master.getBusVoltage() * master.getAppliedOutput());
         namespace.putNumber("current", master::getOutputCurrent);
         namespace.putRunnable("set position", () -> sparkMaxEncoder.setPosition(getAbsolutePosition()));
