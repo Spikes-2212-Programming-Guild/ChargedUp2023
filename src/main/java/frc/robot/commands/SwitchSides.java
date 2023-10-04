04399a2bbc1fd60d77b197cc4537114da06f2d5c
Yoel-Perman-Brilliant <yoyopermam@gmail.com>
2023-10-04 22:13:40 +0300

    removed magic

--- src/main/java/frc/robot/commands/SwitchSides.java
+++ src/main/java/frc/robot/commands/SwitchSides.java
@@ -15,6 +15,11 @@ public class SwitchSides extends SequentialCommandGroup {
 
     private static final Supplier<Double> WAIT_TIME = () -> 0.005;
     private static final Supplier<Double> MOVE_DURATION = () -> 0.8;
+    private static final Supplier<Double> BACK_TOP_POSITION = () -> 185.0;
+    private static final Supplier<Double> FRONT_TOP_POSITION = () -> 5.0;
+    private static final Supplier<Double> SECOND_JOINT_FOLD_DURATION = () -> 1.2;
+    private static final Supplier<Double> FIRST_JOINT_MOVE_TO_MIDDLE_DURATION = () -> 0.8;
+    private static final Supplier<Double> FIRST_JOINT_MIDDLE_POSITION = () -> 90.0;
 
     public SwitchSides(ArmFirstJoint firstJoint, ArmSecondJoint secondJoint, Gripper gripper, boolean isBack) {
         addRequirements(firstJoint, secondJoint);
@@ -25,13 +30,14 @@ public class SwitchSides extends SequentialCommandGroup {
                             WAIT_TIME, MOVE_DURATION),
                     new ParallelRaceGroup(
                             //moves the first joint to a point where the second joint can fold below it
-                            new MoveFirstJoint(firstJoint, () -> 185.0, WAIT_TIME, MOVE_DURATION),
+                            new MoveFirstJoint(firstJoint, BACK_TOP_POSITION, WAIT_TIME, MOVE_DURATION),
                             new KeepSecondJointStable(firstJoint, secondJoint, ArmGravityCompensation.getInstance())
                     ),
                     new MoveSecondJoint(secondJoint, () -> PlaceGamePiece.ArmState.FOLD_ABOVE_180.secondJointPosition,
-                            WAIT_TIME, () -> 1.2),
+                            WAIT_TIME, SECOND_JOINT_FOLD_DURATION),
                     //moves the first joint to the middle
-                    new MoveFirstJoint(firstJoint, () -> 90.0, WAIT_TIME, () -> 0.8),
+                    new MoveFirstJoint(firstJoint, FIRST_JOINT_MIDDLE_POSITION, WAIT_TIME,
+                            FIRST_JOINT_MOVE_TO_MIDDLE_DURATION),
                     new InstantCommand(() -> Drivetrain.getInstance().setMode(CANSparkMax.IdleMode.kCoast))
             );
         } else {
@@ -41,13 +47,14 @@ public class SwitchSides extends SequentialCommandGroup {
                             WAIT_TIME, MOVE_DURATION),
                     new ParallelRaceGroup(
                             //moves the first joint to a point where the second joint can fold below it
-                            new MoveFirstJoint(firstJoint, () -> 5.0, WAIT_TIME, MOVE_DURATION),
+                            new MoveFirstJoint(firstJoint, FRONT_TOP_POSITION, WAIT_TIME, MOVE_DURATION),
                             new KeepSecondJointStable(firstJoint, secondJoint, ArmGravityCompensation.getInstance())
                     ),
                     new MoveSecondJoint(secondJoint, () -> PlaceGamePiece.ArmState.FOLD_BELOW_180.secondJointPosition,
-                            WAIT_TIME, () -> 1.2),
+                            WAIT_TIME, SECOND_JOINT_FOLD_DURATION),
                     //moves the first joint to the middle
-                    new MoveFirstJoint(firstJoint, () -> 90.0, WAIT_TIME, () -> 0.8),
+                    new MoveFirstJoint(firstJoint, FIRST_JOINT_MIDDLE_POSITION,
+                            WAIT_TIME, FIRST_JOINT_MOVE_TO_MIDDLE_DURATION),
                     new InstantCommand(() -> Drivetrain.getInstance().setMode(CANSparkMax.IdleMode.kCoast))
             );
         }
